ad6da48dc7d5ab42543ad7f35305be4e66da3a7f
muhammedyalcin <muhammed.yalcin@digitus.com.tr>
2018-01-26 16:10:45 +0300

    Api geliştirmesi tamamlandı.

--- models/Director.js
+++ models/Director.js
@@ -2,9 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DirectorSchema = new Schema({
-   name: String,
-   surname: String,
-   bio: String,
+   name: {
+       type: String,
+       maxlenght: 60,
+       minlenght: 2
+   },
+   surname:  {
+    type: String,
+    maxlenght: 60,
+    minlenght: 2
+},
+   bio:  {
+    type: String,
+    maxlenght: 1000,
+    minlenght: 2
+},
    createdAt : {
        type : Date,
        default : Date.now
--- models/Movie.js
+++ models/Movie.js
@@ -4,12 +4,30 @@ const Schema = mongoose.Schema;
 const MovieSchema = new Schema({
     title: {
         type:String,
-        required:true,
+        required: [true, '`{PATH}`alanı zorunludur.' ],
+        maxlenght: [15, '`{PATH}` alanı (`{VALUE}`), ({MAXLENGHT}) karakterden küçük olmalıdır.' ],
+        minlenght: [3, '`{PATH}` alanı (`{VALUE}`), ({MINLENGHT}) karakterden büyük olmalıdır. ']
     },
-    category: String,
-    country: String,
-    year:Number,
-    imdbScore: Number,
+    category: {
+        type: String,
+        maxlength: 30,
+        minlenght: 1
+    },
+    country:{
+        type: String,
+        maxlength: 30,
+        minlenght: 1
+    },
+    year:{
+        type: Number,
+        max: 2040,
+        min: 1900
+    },
+    imdbScore: {
+        type: Number,
+        max: 10,
+        min: 0
+    },
     director_id: Schema.Types.ObjectId,
     createdAt: {
         type: Date,
